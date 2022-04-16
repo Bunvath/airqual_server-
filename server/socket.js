b7f4ffb872e7819c7c68f2567c5b5c396aaf1a14
lybunvath <user.emaillybunvath@gmail.com>
2022-04-16 18:02:20 +0100

    formatting the file

---
+++ server/socket.js
@@ -0,0 +1,103 @@
+io = require('./server')
+function Dictionary() {
+    this.dataStore = [];
+    this.add = function (button, status) {
+        this.dataStore.push({
+            button: button,
+            status: status
+        })
+    }
+    this.removeAt = function (button) {
+        for (i = 0; i < this.dataStore.length; i++) {
+            if (this.dataStore[i].button === button) {
+                this.dataStore.splice(this.dataStore[i], 1)
+                return this.dataStore
+            }
+        }
+        return this.dataStore
+    }
+    this.updateAt = function (button, status) {
+        for (i = 0; i < this.dataStore.length; i++) {
+            if (this.dataStore[i].button === button) {
+                this.dataStore[i].status = status
+                return this.dataStore
+            }
+        }
+        return this.dataStore
+    }
+    this.findAt = function (button) {
+        for (i = 0; i < this.dataStore.length; i++) {
+            if (this.dataStore[i].button === button) {
+                return button
+            }
+        }
+        return this.dataStore
+    }
+    this.checkButton = function (button) {
+        i = this.findAt(button)
+        if (i === button) {
+            return true
+        } else {
+            return false
+        }
+
+
+    }
+}
+var dictionary = new Dictionary()
+
+io.on('connection', function (socket) {
+
+    console.log(socket.id + ' connected');
+    socket.emit("connection","you conned to socket server")
+    socket.on('message', function (data) {
+        socket.broadcast.emit('message', data);
+    });
+    socket.on('open_light', data => {
+        console.log("open light")
+        socket.broadcast.emit('open_light', data)
+        var message = JSON.parse(JSON.stringify(data))
+        var button = message.button
+        var status = message.status
+        var exist = dictionary.checkButton(button)
+        if (exist) {
+            dictionary.updateAt(button, status)
+        } else {
+            dictionary.add(button, status)
+        }
+        console.log(dictionary.dataStore)
+    });
+
+    socket.on('init', data => {
+        console.log("calling init event")
+        io.emit('initialize', dictionary.dataStore)
+    })
+    socket.on('data', data => {
+        console.log("socket", data)
+    })
+
+    socket.on('sensor_data', data => {
+        console.log(data)
+        socket.broadcast.emit("sensor_data",data)
+    })
+    socket.on('particle_data',data=>{
+        console.log(data)
+        socket.broadcast.emit("particle_data",data)
+    })
+    socket.on('server', data => {
+        console.log(data);
+    })
+    socket.on('disconnect', data => {
+        console.log(socket.id + " disconnect")
+    })
+    socket.on('manual_light', data => {
+        socket.broadcast.emit("open_light", data)
+        console.log(data)
+    })
+    socket.on('remote', data => {
+        socket.broadcast.emit("remote", data)
+        // console.log(data)
+
+    })
+
+});
